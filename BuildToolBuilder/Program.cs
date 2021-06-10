a19b3cd524ff61a9a6bf59b999a7f2070f96afb6
Measurity <measuring.infinity@gmail.com>
2021-06-10 22:39:42 +0200

    Test additional project to build

---
+++ BuildToolBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildToolBuilder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new Exception("This program should not be executed. It is used as project reference to compile the BuildTool project.");
+        }
+    }
+}
